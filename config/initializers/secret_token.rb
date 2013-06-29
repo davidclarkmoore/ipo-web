e2a7f99dcdc780788658d97749265615649ac451
James Strong <laspluviosillas@gmail.com>
2013-06-29 13:11:30 -0500

    Adding basic rails 3.2 structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IpoWeb::Application.config.secret_token = '65c06c8db99a71c60a5fc53580bb0c152ae155fa4858c20f5f02112d2e3feb0e620141e5ae692d79bf4b63ab7fdd92b604a8128ff7c23fe336713a71be89ff35'

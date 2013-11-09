b7aa03a6ae464fb660f5a0b23e668a91d366d398
James Strong <laspluviosillas@gmail.com>
2013-11-08 20:38:15 -0600

    Add initial attempt to add icons to buttons.
    Uses custom styles for each icon as icons vary in sizes. Hopefully we
    can get all the icons consolidated to one size so this won't be an
    issue.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,23 @@
 module ApplicationHelper
 
+  def image_sprite(image, options = {})
+    sprites = {
+      pencil: {w: 27, h: 27, x: 0, y: 0},
+      arrow_right: {w: 27, h: 23, x: 145, y: 72},
+      search: {w: 32, h: 32, x: 0, y: 72},
+      thumbs_up: {w: 24, h: 29, x: 79, y: 72}
+    }
+    raw %(<span class="sprite #{image}"
+      title="#{options[:title]}"
+      style="background:
+               url(#{path_to_image('/assets/spritesheet.png')}) no-repeat -#{sprites[image][:x]}px -#{sprites[image][:y]}px;
+               width: #{sprites[image][:w]}px;
+               height: #{sprites[image][:h]}px;
+               #{options[:style]}">
+        #{options[:title]}
+      </span>)
+  end
+
   def sort_button name, sort
     current_sort = params[:q].try(:[], "s") || nil
     button_link_to name, url_for(q: {s: sort}), :grey, class: (current_sort == sort) ? 'active' : ''
@@ -18,7 +36,13 @@ module ApplicationHelper
   def button label, color, options={}
     options[:class] ||= ""
     options[:class] += " button button-#{color.to_s}"
-    content_tag(:button, label, options)
+    content_tag(:button, options) do
+      if block_given?
+        yield
+      else
+        label
+      end
+    end
   end
 
   def button_link_to label, href, color, options={}

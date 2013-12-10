4e36868b3080075d005ac7f6980a4e01d546fcbc
Mayra Cabrera <mcabrera1087@gmail.com>
2013-12-10 12:45:32 -0600

    Refactor of ProjectLoader class

--- app/models/project_loader.rb
+++ app/models/project_loader.rb
@@ -1,39 +1,34 @@
 class ProjectLoader
 
 	def self.load_projects(properties)
+
 		return Project.scoped if properties.nil?
-		
+
 		projects = []
 		if properties.has_key?("fields")
-			projects.concat(related_to_fields_of_study?(properties["fields"]))
+			projects.concat(related_to_properties?(properties["fields"], "fields"))
 		elsif properties.has_key?("passions")
-			projects.concat(related_to_student_passions?(properties["passions"]))
+			projects.concat(related_to_properties?(properties["passions"], "passions"))
 		end
 		Project.where(id: projects)
 	end
 
-	def self.related_to_fields_of_study?(study_values)
-    projects = []
-    Project.all.each do |project|
-      field_of_studies = []
-      project.properties["related_fields_of_study"].split(",").each do |field|
-        field_of_studies << field.delete('[]""').strip
-      end
-      projects << project.id unless (field_of_studies & study_values).empty?
-    end
-    projects
-  end
+	def self.related_to_properties?(properties, type)
+		projects = []
+		Project.all.each do |project|
+			project_properties = (type == "fields" ? "related_fields_of_study" : "related_student_passions")
+			fields = get_clean_properties(project, project_properties)
+			projects << project.id unless (fields & properties).empty?
+		end
+		projects
+	end
 
-  def self.related_to_student_passions?(student_passion_values)
-    projects = []
-    Project.all.each do |project|
-      student_passions = []
-      project.properties["related_student_passions"].split(",").each do |field|
-        student_passions << field.delete('[]""').strip
-      end
-      projects << project.id unless (student_passions & student_passion_values).empty?
-    end
-    projects
-  end
+	def self.get_clean_properties(project, project_properties)
+		fields = []
+		project.properties[project_properties].split(",").each do |field|
+			fields << field.delete('[]""').strip
+		end
+		fields
+	end
 
 end

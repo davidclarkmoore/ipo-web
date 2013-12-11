88526d3423cf836832f48ee50cbace7cf8698954
Mayra Cabrera <mcabrera1087@gmail.com>
2013-12-10 20:06:24 -0600

    First specs fo projects controller

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -7,6 +7,62 @@ describe ProjectsController do
       get 'index'
       response.should be_success
     end
+
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+    end
+
+    it "returns all projects if no filter is used" do
+      total = rand(1..10)
+      FactoryGirl.create_list(:project, total, :complete)
+      
+      get :index    
+      expect(assigns(:total_projects)).to eq(total)
+    end
+
+    describe "Search projects" do
+      context "Using name" do
+        before(:each) do
+          @total = rand(1..10)
+          FactoryGirl.create_list(:project, rand(1..10), :complete)
+          FactoryGirl.create_list(:project, @total, :static_name, :complete)
+        end
+
+        it "should return only projects that match" do
+          get :index, q: {"name_cont"=>"project"}
+          expect(assigns(:total_projects)).to eq(@total)
+        end
+
+        it "should return nothing it no project match" do 
+          get :index, q: {"name_cont" => "asdfg"}
+          expect(assigns(:total_projects)).to eq(0)
+        end
+      end
+
+      context "Using team mode" do
+        before(:each) do
+          @total_individual = rand(1..10)
+          FactoryGirl.create_list(:project, rand(1..10), :complete)
+          FactoryGirl.create_list(:project, @total_individual, :individual_mode, :complete)
+        end
+
+         it "should return only team projects" do
+          get :index, q: {"team_mode_true" => 1}
+          expect(assigns(:total_projects)).to eq(Project.all.count - @total_individual)
+         end
+
+         it "should return only individual projects" do
+          get :index, q: {"team_mode_true" => 0, "team_mode_false" => 1}
+          expect(assigns(:total_projects)).to eq(@total_individual)
+         end
+      end
+
+      context "Using session"
+      context "Using fields of study"
+      context "Using fields by passion"
+      context "Using mixing filters"
+    end
   end
 
 end
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,8 +1,40 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
   factory :project do
     name { Faker::Lorem.words(3).join.capitalize }
     description { Faker::Lorem.paragraphs 2 }
+    team_mode true
+    location_private true
+  end
+
+  trait :static_name do
+    name { Faker::Lorem.words(3).join.capitalize + "_project" }
+  end
+
+  trait :individual_mode do
+    team_mode false
+  end
+
+  trait :complete do
+    wizard_status "complete"
+    properties {{
+      "student_educational_requirement" => "Graduate Degree (MA/MS)",
+      "min_students" => rand(1..2), 
+      "max_students" => rand(3..10),
+      "location" => Faker::Address.city,
+      "address" => Faker::Address.street_name,
+      "internet_distance" => "On-site (Free)",
+      "location_description" => Faker::Lorem.sentence(10),
+      "culture_description" => Faker::Lorem.sentence(10),
+      "housing_type" => "Dormitory",
+      "dining_location" => "Cafeteria",
+      "housing_description" => Faker::Lorem.sentence(10),
+      "safety_level" => "Never Walk Alone",
+      "challenges_description" => Faker::Lorem.sentence(10),
+      "typical_attire" => "Very Modest",
+      "guidelines_description" => Faker::Lorem.sentence(10),
+      "agree_memo" => "true",
+      "agree_to_transport" => "true"
+    }}
   end
 end

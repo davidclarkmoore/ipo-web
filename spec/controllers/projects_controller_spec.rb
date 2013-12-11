53c95aa9630ccf0d8918724b18a7903ad6f9e446
Mayra Cabrera <mcabrera1087@gmail.com>
2013-12-11 11:58:30 -0600

    Add specs to validate different filters in project search

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -15,7 +15,7 @@ describe ProjectsController do
 
     it "returns all projects if no filter is used" do
       total = rand(1..10)
-      create_list(:project, total, :complete)
+      create_list(:completed_projects, total)
       
       get :index    
       expect(assigns(:total_projects)).to eq(total)
@@ -25,8 +25,8 @@ describe ProjectsController do
       context "Using name" do
         before(:each) do
           @total = rand(1..10)
-          create_list(:project, rand(1..10), :complete)
-          create_list(:project, @total, :static_name, :complete)
+          create_list(:completed_projects, rand(1..10))
+          create_list(:projects_with_static_name, @total)
         end
 
         it "should return only projects that match" do
@@ -34,7 +34,7 @@ describe ProjectsController do
           expect(assigns(:total_projects)).to eq(@total)
         end
 
-        it "should return nothing it no project match" do 
+        it "should return nothing if no project match" do 
           get :index, q: {"name_cont" => "asdfg"}
           expect(assigns(:total_projects)).to eq(0)
         end
@@ -43,8 +43,8 @@ describe ProjectsController do
       context "Using team mode" do
         before(:each) do
           @total_individual = rand(1..10)
-          create_list(:project, rand(1..10), :complete)
-          create_list(:project, @total_individual, :individual_mode, :complete)
+          create_list(:completed_projects, rand(1..10))
+          create_list(:individual_projects, @total_individual)
         end
 
          it "should return only team projects" do
@@ -58,11 +58,50 @@ describe ProjectsController do
          end
       end
 
-      context "Using session"
-      context "Using fields of study"
+      context "Using session timeframe" do
+        before(:each) do
+          @total = rand(1..10)
+          create_list(:completed_projects, rand(1..10), created_at: Date.tomorrow)
+          create_list(:completed_projects, @total, created_at: Date.today - 1.month)
+        end
+
+        it "should return projects in a range of dates" do
+          created_at_gt = (Date.today - 1.month).strftime("%Y/%m/%d")
+          created_at_lt = (Date.today).strftime("%Y/%m/%d")
+          get :index, q: {"created_at_gt"=> created_at_gt, "created_at_lt"=>created_at_lt}
+          expect(assigns(:total_projects)).to eq(@total)
+        end
+
+        it "should return nothing if no project is created in the specified interval" do
+          created_at_gt = (Date.today + 1.month).strftime("%Y/%m/%d")
+          get :index, q: {"created_at_gt" => created_at_gt}
+          expect(assigns(:total_projects)).to eq(0)
+        end
+      end
+
+      context "Using fields of study" do
+        before(:each) do
+          @total = rand(1..10)
+          projects = create_list(:completed_projects, @total)
+          create_list(:completed_projects, rand(1..10))
+          fields = "[\"English\", \"Web Design\", \"Psychology\"]"
+          set_different_fields_of_study(projects, fields)
+        end
+
+        it "should return projects that contain the related field of study" do
+          get :index, properties: {"fields"=>["Web Design"]}
+          expect(assigns(:total_projects)).to eq(@total)
+        end
+      end
       context "Using fields by passion"
       context "Using mixing filters"
     end
   end
 
+  def set_different_fields_of_study(projects, fields_of_study)
+    projects.each do |project|
+      project.properties.merge!("related_fields_of_study" => fields_of_study)
+      project.save
+    end
+  end
 end
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -5,36 +5,45 @@ FactoryGirl.define do
     description { Faker::Lorem.paragraphs 2 }
     team_mode true
     location_private true
-  end
 
-  trait :static_name do
-    name { Faker::Lorem.words(3).join.capitalize + "_project" }
-  end
+    trait :static_name do
+      name { Faker::Lorem.words(3).join.capitalize + "_project" }
+    end
 
-  trait :individual_mode do
-    team_mode false
-  end
+    trait :individual_mode do
+      team_mode false
+    end
 
-  trait :complete do
-    wizard_status "complete"
-    properties {{
-      "student_educational_requirement" => "Graduate Degree (MA/MS)",
-      "min_students" => rand(1..2), 
-      "max_students" => rand(3..10),
-      "location" => Faker::Address.city,
-      "address" => Faker::Address.street_name,
-      "internet_distance" => "On-site (Free)",
-      "location_description" => Faker::Lorem.sentence(10),
-      "culture_description" => Faker::Lorem.sentence(10),
-      "housing_type" => "Dormitory",
-      "dining_location" => "Cafeteria",
-      "housing_description" => Faker::Lorem.sentence(10),
-      "safety_level" => "Never Walk Alone",
-      "challenges_description" => Faker::Lorem.sentence(10),
-      "typical_attire" => "Very Modest",
-      "guidelines_description" => Faker::Lorem.sentence(10),
-      "agree_memo" => "true",
-      "agree_to_transport" => "true"
-    }}
+    trait :complete do
+      wizard_status "complete"
+    end
+
+    trait :with_properties do
+      properties {{
+        "student_educational_requirement" => "Graduate Degree (MA/MS)",
+        "min_students" => rand(1..2), 
+        "max_students" => rand(3..10),
+        "location" => Faker::Address.city,
+        "address" => Faker::Address.street_name,
+        "internet_distance" => "On-site (Free)",
+        "location_description" => Faker::Lorem.sentence(10),
+        "culture_description" => Faker::Lorem.sentence(10),
+        "housing_type" => "Dormitory",
+        "dining_location" => "Cafeteria",
+        "housing_description" => Faker::Lorem.sentence(10),
+        "safety_level" => "Never Walk Alone",
+        "challenges_description" => Faker::Lorem.sentence(10),
+        "typical_attire" => "Very Modest",
+        "guidelines_description" => Faker::Lorem.sentence(10),
+        "agree_memo" => "true",
+        "agree_to_transport" => "true",
+        "related_fields_of_study" => "[\"Aviation\", \"Counseling\", \"Social Work\"]",
+        "related_student_passions" => "[\"Poverty\", \"Other\"]"
+      }}
+    end
+
+    factory :completed_projects,            traits: [:complete, :with_properties]
+    factory :projects_with_static_name,     traits: [:complete, :static_name, :with_properties]
+    factory :individual_projects,           traits: [:complete, :individual_mode, :with_properties]
   end
 end

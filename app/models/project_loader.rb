a0763c978d8149420ce553b53caec88e16be6c3e
Mayra Cabrera <mcabrera1087@gmail.com>
2013-12-11 18:53:54 -0600

    Refactor over ProjectLoader class

--- app/models/project_loader.rb
+++ app/models/project_loader.rb
@@ -2,21 +2,18 @@ class ProjectLoader
 
 	def self.load_projects(properties)
 		return Project.scoped if properties.nil?
-
 		projects = []
-		if properties.has_key?("fields")
-			projects.concat(related_to_properties?(properties["fields"], "fields"))
-		elsif properties.has_key?("passions")
-			projects.concat(related_to_properties?(properties["passions"], "passions"))
+
+		properties.each do |key, value|
+			projects.concat(related_to_properties?(key, value))
 		end
 		Project.where(id: projects)
 	end
 
-	def self.related_to_properties?(properties, type)
+	def self.related_to_properties?(type, properties)
 		projects = []
-		Project.all.each do |project|
-			project_properties = (type == "fields" ? "related_fields_of_study" : "related_student_passions")
-			fields = get_clean_properties(project, project_properties)
+		Project.all.each do |project|	
+			fields = get_clean_properties(project, type)
 			projects << project.id unless (fields & properties).empty?
 		end
 		projects

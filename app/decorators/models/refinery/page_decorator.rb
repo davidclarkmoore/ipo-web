3003fd825b8efd7a183890c5b8b48af385b14f3d
James Strong <laspluviosillas@gmail.com>
2014-01-12 13:18:51 -0600

    Hide submenus if they are set to show_in_menu: false

--- app/decorators/models/refinery/page_decorator.rb
+++ app/decorators/models/refinery/page_decorator.rb
@@ -1,8 +1,8 @@
 Refinery::Page.class_eval do
   scope :in_main_menu, -> { where(show_in_menu: true, parent_id: nil) }
-  scope :pages_related, lambda { |parent_page| where(parent_id: parent_page) }
+  scope :pages_related, lambda { |parent_page| where(parent_id: parent_page, show_in_menu: true) }
 
-  
+
   def has_nested_pages?
     !Refinery::Page.pages_related(self.id).empty?
   end

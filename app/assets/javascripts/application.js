63c197a5dc3583051e19f21a473b35ee4965663a
David Clark Moore <davidclarkmoore@gmail.com>
2014-02-02 14:26:32 -0500

    Added Flat UI Checkbox JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require flatui-checkbox
+//= require flatui-radio
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.autocomplete

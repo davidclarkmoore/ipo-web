f53ce2cfa8040e8585103995519e0b068dc9bbd0
James Strong <laspluviosillas@gmail.com>
2014-02-17 13:19:47 -0600

    Fix broken migration.
    Application deadline is never created when migrating from scratch.
    Probably a remnant from a now non-existing migration.

--- db/migrate/20140211153034_remove_fields_from_project_session.rb
+++ db/migrate/20140211153034_remove_fields_from_project_session.rb
@@ -3,7 +3,6 @@ class RemoveFieldsFromProjectSession < ActiveRecord::Migration
     remove_column :project_sessions, :start_date
     remove_column :project_sessions, :end_date
     remove_column :project_sessions, :title
-    remove_column :project_sessions, :application_deadline
     add_column    :project_sessions, :session_id, :integer
   end
 
@@ -11,7 +10,6 @@ class RemoveFieldsFromProjectSession < ActiveRecord::Migration
     add_column :project_sessions, :start_date, :date
     add_column :project_sessions, :end_date, :date
     add_column :project_sessions, :title, :string
-    add_column :project_sessions, :application_deadline, :date
     remove_column :project_sessions, :session_id
   end
 end

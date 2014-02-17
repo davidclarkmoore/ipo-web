b18f2907d2e029e5da4e30ca52dc93bacfd4f7bb
Mayra Cabrera <mcabrera1087@gmail.com>
2014-02-17 10:17:40 -0600

    Basic specs for controllers

--- spec/controllers/students_setup_controller_spec.rb
+++ spec/controllers/students_setup_controller_spec.rb
@@ -2,4 +2,27 @@ require 'spec_helper'
 
 describe StudentsSetupController do
 
+  before(:each) do
+    signin
+  end
+  
+  describe "GET 'index'" do
+    it "redirects to first step" do
+      get 'index'
+      response.should redirect_to students_setup_path('about_you')
+    end
+  end
+
+  describe "GET '#show about_you'" do
+    it "returns http success" do
+      get :show, id: 'about_you'
+      response.should be_success
+    end
+
+    it 'renders the about_you template' do
+      get :show, id: 'about_you'
+      response.should render_template("about_you")
+    end
+  end
+
 end

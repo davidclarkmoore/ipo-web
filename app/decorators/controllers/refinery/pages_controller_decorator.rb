907e093a5e21767e2ca034e9c7b61e0881fdd5f4
Aldo Solis <aldosolis@gmail.com>
2014-03-20 13:52:46 -0600

    Create a seed to create a home page for a fresh installation

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,6 +1,6 @@
 Refinery::PagesController.class_eval do
     
-    before_filter :fill_fields_of_study_and_passion_areas, :only => [:show]
+    before_filter :fill_fields_of_study_and_passion_areas, :only => [:home]
 
 	protected
 

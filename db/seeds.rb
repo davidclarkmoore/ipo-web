e87cda1b560ffbc3e6724629bc54a094fbde65f3
Aldo Solis <aldosolis@gmail.com>
2014-03-20 18:06:03 -0600

    Add text field to edit the page part title in refinery admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,78 +35,78 @@ sharing_options = Refinery::Image.last
 
 
 Refinery::PagePart.create!([
-	{ 
-		refinery_page_id: home_page.id, 
-	  	title: "Search", 
-	  	body: "<h2>Don't see your field? <br />Search all of our projects. Boom, its that easy.</h2>", 
-	  	position: 3
-	},
-	{ 
-		refinery_page_id: home_page.id, 
-		title: "Ipo", 
-		body: "<img src=\"%s\" title=\"Vid Thumb1\" alt=\"Vid Thumb1\" width=\"487\" height=\"297\" />\r\n<h1 class=\"text-align-left\"><span style=\"background-color: transparent;\" class=\"font-size-normal\">What makes IPO different?</span>\r\n</h1>\r\n<div><div><h2><span>Choose a missional internship that matches your field of study,&#160;area of passion, and availability. Because sometimes you just need to get out of the classroom.</span>\r\n</h2>\r\n</div>\r\n</div>" % vid_thumb.url, 
-		position: 4, 
-		display_type: "img-right"
-	},
-	{
-		refinery_page_id: home_page.id, 
-		title: "Stories Title", 
-		body: "<p>This could be you! Hear stories from the field.</p>",
-		position: 6
-	}, 
-	{
-		refinery_page_id: home_page.id, 
-		title: "Story1", 
-		body: "<p><img src=\"%s\" title=\"Field Experience 1\" alt=\"Field Experience 1\" data-rel=\"225x255\" width=\"244\" height=\"153\" /></p>\r\n<h1>Xianyi Wu</h1>\r\n<p>One student who changed 17 million lives</p>" % field_experience.url,  
-		position: 7
-	}, 
-	{
-		refinery_page_id: home_page.id, 
-		title: "Story2", 
-		body: "<p><img src=\"%s\" title=\"Field Experience 1\" alt=\"Field Experience 1\" data-rel=\"225x255\" width=\"244\" height=\"153\" /></p>\r\n<h1>Xianyi Wu</h1>\r\n<p>One student who changed 17 million lives</p>" % field_experience.url,  
-		position: 8
-	}, 
-	{
-		refinery_page_id: home_page.id, 
-		title: "Story3", 
-		body: "<p><img src=\"%s\" title=\"Field Experience 1\" alt=\"Field Experience 1\" data-rel=\"225x255\" width=\"244\" height=\"153\" /></p>\r\n<h1>Xianyi Wu</h1>\r\n<p>One student who changed 17 million lives</p>" % field_experience.url,  
-		position: 9
-	}, 
-	{
-		refinery_page_id: home_page.id, 
-		title: "Story4", 
-		body: "<p><img src=\"%s\" title=\"Field Experience 1\" alt=\"Field Experience 1\" data-rel=\"225x255\" width=\"244\" height=\"153\" /></p>\r\n<h1>Xianyi Wu</h1>\r\n<p>One student who changed 17 million lives</p>" % field_experience.url,  
-		position: 10
-	}, 
-	{
-		refinery_page_id: home_page.id, 
-		title: "Motto", 
-		body: "<p class=\"text-align-center\"><img src=\"#{lightbulbs.url}\" title=\"Lightbulbs\" alt=\"Lightbulbs\" data-rel=\"225x255\" width=\"268\" height=\"104\" /></p>\r\n<h2 class=\"text-align-center\">Customized, Missional, Interships</h2>\r\n<p class=\"text-align-center\">70% of Christ's followers live in the Global South.&#160;70% of Christ's followers live in the Global South.&#160;70% of Christ's followers live in the Global South.</p>",  
-		position: 2
-	}, 
-	{
-		refinery_page_id: home_page.id, 
-		title: "Internship", 
-		body: "<img src=\"%s\" title=\"Infographic\" alt=\"Infographic\" data-rel=\"225x255\" width=\"428\" height=\"371\" />\r\n<h1>A global internship model for the 21st century</h1>\r\n<h2>Choose a missional internship that matches your field of study, area of passion, and availability. Because sometimes you just need to get out of the classroom.</h2>\r\n<h3>Truly global internships</h3>\r\n<p>Lorem ipsum is simply dummy text of the printing and typesetting industry.</p>\r\n<h3>Your major, outside the classroom</h3>\r\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>\r\n<h3>Virtual Cross-Cultural Missions Training</h3>\r\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>\r\n<h3>Amazing, Commited Mentors</h3>\r\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>" % infographic.url, 
-		position: 5,
-		display_type: "img-left"
-	}, 
-	{
-		refinery_page_id: home_page.id, 
-		title: "Share", 
-		body: "<h2>Share this!</h2>\r\n<p><img src=\"%s\" title=\"Sharing Options\" alt=\"Sharing Options\" data-rel=\"225x255\" width=\"275\" height=\"65\" /></p>" % sharing_options.url,
-		position: 11
-	}, 
-	{ 
-		refinery_page_id: home_page.id, 
-		title: "Events", 
-		body: "<h2>Events</h2>\r\n<h3>Check out one of our  mobilization events, webinars, or fundraisers!</h3>",
-		position: 12
-	}, 
-	{
-		refinery_page_id: home_page.id, 
-		title: "Support", 
-		body: "<h2>Support IPO</h2>\r\n<h3>Give to students &amp; projects or tell the World about us!</h3>",
-		position: 13
-	}])
+  { 
+    refinery_page_id: home_page.id, 
+      title: "Search", 
+      body: "<h2>Don't see your field? <br />Search all of our projects. Boom, its that easy.</h2>", 
+      position: 3
+  },
+  { 
+    refinery_page_id: home_page.id, 
+    title: "Ipo", 
+    body: "<img src=\"%s\" title=\"Vid Thumb1\" alt=\"Vid Thumb1\" width=\"487\" height=\"297\" />\r\n<h1 class=\"text-align-left\"><span style=\"background-color: transparent;\" class=\"font-size-normal\">What makes IPO different?</span>\r\n</h1>\r\n<div><div><h2><span>Choose a missional internship that matches your field of study,&#160;area of passion, and availability. Because sometimes you just need to get out of the classroom.</span>\r\n</h2>\r\n</div>\r\n</div>" % vid_thumb.url, 
+    position: 4, 
+    display_type: "img-right"
+  },
+  {
+    refinery_page_id: home_page.id, 
+    title: "Stories Title", 
+    body: "<p>This could be you! Hear stories from the field.</p>",
+    position: 6
+  }, 
+  {
+    refinery_page_id: home_page.id, 
+    title: "Story1", 
+    body: "<p><img src=\"%s\" title=\"Field Experience 1\" alt=\"Field Experience 1\" data-rel=\"225x255\" width=\"244\" height=\"153\" /></p>\r\n<h1>Xianyi Wu</h1>\r\n<p>One student who changed 17 million lives</p>" % field_experience.url,  
+    position: 7
+  }, 
+  {
+    refinery_page_id: home_page.id, 
+    title: "Story2", 
+    body: "<p><img src=\"%s\" title=\"Field Experience 1\" alt=\"Field Experience 1\" data-rel=\"225x255\" width=\"244\" height=\"153\" /></p>\r\n<h1>Xianyi Wu</h1>\r\n<p>One student who changed 17 million lives</p>" % field_experience.url,  
+    position: 8
+  }, 
+  {
+    refinery_page_id: home_page.id, 
+    title: "Story3", 
+    body: "<p><img src=\"%s\" title=\"Field Experience 1\" alt=\"Field Experience 1\" data-rel=\"225x255\" width=\"244\" height=\"153\" /></p>\r\n<h1>Xianyi Wu</h1>\r\n<p>One student who changed 17 million lives</p>" % field_experience.url,  
+    position: 9
+  }, 
+  {
+    refinery_page_id: home_page.id, 
+    title: "Story4", 
+    body: "<p><img src=\"%s\" title=\"Field Experience 1\" alt=\"Field Experience 1\" data-rel=\"225x255\" width=\"244\" height=\"153\" /></p>\r\n<h1>Xianyi Wu</h1>\r\n<p>One student who changed 17 million lives</p>" % field_experience.url,  
+    position: 10
+  }, 
+  {
+    refinery_page_id: home_page.id, 
+    title: "Motto", 
+    body: "<p class=\"text-align-center\"><img src=\"#{lightbulbs.url}\" title=\"Lightbulbs\" alt=\"Lightbulbs\" data-rel=\"225x255\" width=\"268\" height=\"104\" /></p>\r\n<h2 class=\"text-align-center\">Customized, Missional, Interships</h2>\r\n<p class=\"text-align-center\">70% of Christ's followers live in the Global South.&#160;70% of Christ's followers live in the Global South.&#160;70% of Christ's followers live in the Global South.</p>",  
+    position: 2
+  }, 
+  {
+    refinery_page_id: home_page.id, 
+    title: "Internship", 
+    body: "<img src=\"%s\" title=\"Infographic\" alt=\"Infographic\" data-rel=\"225x255\" width=\"428\" height=\"371\" />\r\n<h1>A global internship model for the 21st century</h1>\r\n<h2>Choose a missional internship that matches your field of study, area of passion, and availability. Because sometimes you just need to get out of the classroom.</h2>\r\n<h3>Truly global internships</h3>\r\n<p>Lorem ipsum is simply dummy text of the printing and typesetting industry.</p>\r\n<h3>Your major, outside the classroom</h3>\r\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>\r\n<h3>Virtual Cross-Cultural Missions Training</h3>\r\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>\r\n<h3>Amazing, Commited Mentors</h3>\r\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>" % infographic.url, 
+    position: 5,
+    display_type: "img-left"
+  }, 
+  {
+    refinery_page_id: home_page.id, 
+    title: "Share", 
+    body: "<h2>Share this!</h2>\r\n<p><img src=\"%s\" title=\"Sharing Options\" alt=\"Sharing Options\" data-rel=\"225x255\" width=\"275\" height=\"65\" /></p>" % sharing_options.url,
+    position: 11
+  }, 
+  { 
+    refinery_page_id: home_page.id, 
+    title: "Events", 
+    body: "<h2>Events</h2>\r\n<h3>Check out one of our  mobilization events, webinars, or fundraisers!</h3>",
+    position: 12
+  }, 
+  {
+    refinery_page_id: home_page.id, 
+    title: "Support", 
+    body: "<h2>Support IPO</h2>\r\n<h3>Give to students &amp; projects or tell the World about us!</h3>",
+    position: 13
+  }])
 

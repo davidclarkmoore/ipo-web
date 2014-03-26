b4fc9fc60788b6c37f252394398eae3bb9f140f2
Aldo Solis <aldosolis@gmail.com>
2014-03-26 17:24:08 -0600

    Refactor for project_spec.rb

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -7,51 +7,41 @@ describe Project do
 
   let (:project) { Project.new }
 
-  def set_status_and_test(status)
+  def test_with_status(status)
     project.wizard_status = status
     expect(yield).to be_true
   end
 
   it "#complete? should return true if status == 'complete'" do
-    set_status_and_test('complete') { project.complete? }
+    test_with_status('complete') { project.complete? }
   end
 
   it "#complete_or_about_you? should return true if status == 'about_you' or status == 'complete'" do
-    project.wizard_status = 'complete'
-    expect(project.complete_or_about_you?).to be_true
-    project.wizard_status = 'about_you'
-    expect(project.complete_or_about_you?).to be_true
+    test_with_status('complete') { project.complete_or_about_you? }
+    test_with_status('about_you') { project.complete_or_about_you? }
   end
 
   it "#complete_or_the_project? should return true if status = 'the_project' or status == 'complete'" do
-    project.wizard_status = 'complete'
-    expect(project.complete_or_the_project?).to be_true
-    project.wizard_status = 'the_project'
-    expect(project.complete_or_the_project?).to be_true
+    test_with_status('complete') { project.complete_or_the_project? }
+    test_with_status('the_project') { project.complete_or_the_project? }
   end
 
   it "#complete_or_location? should return true if status = 'location' or status == 'complete'" do
-    project.wizard_status = 'complete'
-    expect(project.complete_or_location?).to be_true
-    project.wizard_status = 'location'
-    expect(project.complete_or_location?).to be_true
+    test_with_status('complete') { project.complete_or_location? }
+    test_with_status('location') { project.complete_or_location? }
   end
 
   it "#complete_or_content? should return true if status = 'content' or status == 'complete'" do
-    project.wizard_status = 'complete'
-    expect(project.complete_or_content?).to be_true
-    project.wizard_status = 'content'
-    expect(project.complete_or_content?).to be_true
+    test_with_status('complete') { project.complete_or_content? }
+    test_with_status('content') { project.complete_or_content? }
   end
 
   it "#complete_or_agreement? should return true if status = 'agreement' or status == 'complete'" do
-    project.wizard_status = 'complete'
-    expect(project.complete_or_agreement?).to be_true
-    project.wizard_status = 'agreement'
-    expect(project.complete_or_agreement?).to be_true
+    test_with_status('complete') { project.complete_or_agreement? }
+    test_with_status('agreement') { project.complete_or_agreement? }
   end
 
-  it "#get_pretty_properties should return a comma separated list of data" do
+  it "#get_pretty_properties should return a comma separated list for a key in locale" do
     stub(I18n).t { {a: 'a', b: 'b', c: 'c'} }
     expect(project.get_pretty_properties(['a', 'b', 'c'], 'key')).to eq("a, b, c")
   end

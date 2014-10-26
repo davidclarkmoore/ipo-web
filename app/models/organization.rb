7b160fc475d2a269cbb3ee870b6c6ff05af66dce
David Clark Moore <davidclarkmoore@gmail.com>
2014-10-26 16:44:29 -0400

    update to add description to org

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,13 +3,13 @@ class Organization < ActiveRecord::Base
   include SFRails::ActiveRecord
   salesforce "Account", [:name, :website], {organization_type: "Type"}
 
-  enumerize :organization_type, 
+  enumerize :organization_type,
             in: I18n.t("enumerize.organization.organization_type")
 
   has_many :field_hosts
   has_many :projects
 
-  attr_accessible :name, :organization_type, :website
+  attr_accessible :name, :organization_type, :website, :description
 
   validates_presence_of :name, :organization_type
 end

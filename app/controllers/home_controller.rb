bda24d1c60bf4818244b434c68770badf42240c2
Aldo Solis <aldosolis@gmail.com>
2014-10-27 23:00:18 -0600

    Ensure that pages in Draft or Hidden in Refinery don't show up in Elastic Search/Page Search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,15 +8,28 @@ class HomeController < ApplicationController
   
   def wide_search
     @search = params[:search]
-    @pages = Refinery::Page.search(@search, :autocomplete => false,:fields => [:title, :bodies], :highlight => true)
-    @projects = Project.search(@search, :autocomplete => false,:fields => [:name, :description], :highlight => true)
+    @pages = Refinery::Page.search(@search, :autocomplete => false, :fields => [:title, :bodies], :highlight => true, 
+      :where => { show_in_menu: true, draft: false})
+    
+    #this hack solves the problem with enumerized status, 
+    #since where: { status: 'approved'} was not returning any result
+    @projects = Project.search(@search, :autocomplete => false, :fields => [:name, :description], :highlight => true,
+      :where => { id: { in: project_approved_ids }})  
   end
   
   def elastic_autocomplete
     @search = params[:term]
-    pages = Refinery::Page.search(@search, :autocomplete => true,:fields => [:title], :highlight => false).map(&:title)
-    projects = Project.search(@search, :autocomplete => true,:fields => [:name], :highlight => false).map(&:name)
+    pages = Refinery::Page.search(@search, :autocomplete => true,:fields => [:title], :highlight => false, 
+      :where => { show_in_menu: true, draft: false}).map(&:title)
+    
+    #this hack solves the problem with enumerized status, 
+    #since where: { status: 'approved'} was not returning any result
+    projects = Project.search(@search, :autocomplete => true,:fields => [:name], :highlight => false,
+      :where => { id: { in: project_approved_ids }}).map(&:name)
     render json: pages+projects
   end
-  
+
+  def project_approved_ids
+    @ids ||= Project.approved.pluck(:id)
+  end
 end

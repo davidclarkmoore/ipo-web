8904582763d08a6bd4e04e031f3bf1f487a9cf9d
David Clark Moore <davidclarkmoore@gmail.com>
2014-11-02 16:29:40 -0500

    basic org page with listing of org projects and form

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,12 +1,44 @@
 class OrganizationsController < ApplicationController
 
   def index
-    @o = Organization.find(params[:id])
+
   end
 
   def show
     @o = Organization.find(params[:id])
 
+    @projects = Project.where organization_id: @o.id
+
+  end
+
+  def new
+    @o = Organization.new
+
+    respond_to do |format|
+      format.html  # new.html.erb
+      format.json  { render :json => @o }
+    end
+  end
+
+  def create
+    org = Organization.new(params[:organization])
+    org.save
+    redirect_to :organizations
+  end
+
+  def edit
+    @o = Organization.find(params[:id])
+  end
+
+  def update
+
+  	org = Organization.find params[:id]
+
+		if org.update_attributes params[:post]
+		redirect_to organization_path
+		else
+		redirect_to :organizations
+		end
   end
 
 end

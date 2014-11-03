d82d4bd3389e40042171d53810dfd8bb057ce34d
David Clark Moore <davidclarkmoore@gmail.com>
2014-11-02 19:28:48 -0500

    remove all spec lessons work

--- spec/factories/lessions.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :lession do
-    title "MyString"
-    description "MyText"
-    video "MyText"
-  end
-end
